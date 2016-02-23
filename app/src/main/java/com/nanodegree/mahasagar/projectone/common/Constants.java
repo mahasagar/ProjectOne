48c9ae288a009ab55ff7d1f9b2de01593703c0c2
sagar <sagar.chaudhari@medibox.in>
2016-02-24 00:44:54 +0530

    FrameLayout review updated

--- app/src/main/java/com/nanodegree/mahasagar/projectone/common/Constants.java
+++ app/src/main/java/com/nanodegree/mahasagar/projectone/common/Constants.java
@@ -5,7 +5,7 @@ package com.nanodegree.mahasagar.projectone.common;
  */
 public class Constants {
 
-    public static final String API_KEY = "****** Enter Ur API KEY Here *********";
+    public static final String API_KEY = "382b15ec50c5e60faff2089c899e2448";
     public static final String URL_POPULARITY = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="+API_KEY +"#results/7";
     public static final String URL_VOTES="http://api.themoviedb.org/3/discover/movie?sort_by=vote_count.desc&api_key="+API_KEY+"#results/7";
     public static final String IMG_URL = "http://image.tmdb.org/t/p/w500/";
